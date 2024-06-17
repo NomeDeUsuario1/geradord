6c9c0ad9738c5f34e8e73be5aad9d0befc3e08d4
NomeDeUsuario1 <129603914+NomeDeUsuario1@users.noreply.github.com>
2024-06-17 11:49:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJkLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '1234567890';
+const simbolos = '!@%*?';
